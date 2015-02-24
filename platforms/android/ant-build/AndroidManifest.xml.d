d39e7a67ad9e041d6ab3e926e9a2e4e7e316006f
pratik pancahl <pratik.panchal@infostretch.com>
2015-02-24 12:00:49 +0530

    correct icon

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\xampp\htdocs\demorepo\platforms\android\ant-build\AndroidManifest.xml : \
-C:\xampp\htdocs\demorepo\platforms\android\AndroidManifest.xml \
+c:\xampp\htdocs\demorepo\platforms\android\ant-build\AndroidManifest.xml : \
+c:\xampp\htdocs\demorepo\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-C:\xampp\htdocs\demorepo\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\xampp\htdocs\demorepo\platforms\android\ant-build\CordovaApp.ap_ \
-C:\xampp\htdocs\demorepo\platforms\android\ant-build\classes.dex \
+c:\xampp\htdocs\demorepo\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+c:\xampp\htdocs\demorepo\platforms\android\ant-build\CordovaApp.ap_ \
+c:\xampp\htdocs\demorepo\platforms\android\ant-build\classes.dex \
 C:\xampp\htdocs\demorepo\platforms\android\CordovaLib\ant-build\classes.jar \
